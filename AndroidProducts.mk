b4817deec6b3d0e3f591dc82076cce18b6fdc341
TheBeast <88646493+xootkama@users.noreply.github.com>
2022-01-08 21:27:08 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_kebab.mk
+    $(LOCAL_DIR)/evolution_kebab.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_kebab-user \
-    lineage_kebab-userdebug \
-    lineage_kebab-eng
+    evolution_kebab-user \
+    evolution_kebab-userdebug \
+    evolution_kebab-eng
